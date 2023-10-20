9956385d60b35991e3c18f460b6e7c5bf7cdc912
deathwoods <deathwoods902@gmail.com>
2023-10-21 02:03:32 +0700

    map2

--- Assets/Gong's Folder/EmemyController.cs
+++ Assets/Gong's Folder/EmemyController.cs
@@ -19,30 +19,42 @@ public class EmemyController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        numberOfEnemiesToSpawn = DayCount.days;
+        //numberOfEnemiesToSpawn = DayCount.days;
 
     }
 
     IEnumerator SpawnEnemys()
     {
+        while (true)
+        {
+            SpawnEnemies();
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    void SpawnEnemies()
+    {
+
         for (int i = 0; i < numberOfEnemiesToSpawn; i++)
         {
             Vector2 randomSpawnPoint = new Vector2(
                 Random.Range(spawnArea.position.x - spawnArea.localScale.x / 2, spawnArea.position.x + spawnArea.localScale.x / 2),
-                Random.Range(spawnArea.position.y - spawnArea.localScale.y / 2, spawnArea.position.y + spawnArea.localScale.y / 2));
+                spawnArea.position.y
+            );
 
             int enemynum = Random.Range(0, 100);
             if (enemynum > 50)
             {
-                Instantiate(meleeEnemyPrefab, randomSpawnPoint, Quaternion.identity) ;
+                Instantiate(meleeEnemyPrefab, randomSpawnPoint, Quaternion.identity);
             }
             if (enemynum < 49)
             {
                 Instantiate(rangeEnemyPrefab, randomSpawnPoint, Quaternion.identity);
             }
         }
-        yield return new WaitForSeconds(10f);
+
     }
 
-    
+
+
 }
